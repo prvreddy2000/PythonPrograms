3906799fcbdad52e1fe38d24207be0d627091666
prvreddy2000 <prvreddy2000@gmail.com>
2023-06-18 06:30:23 +0530

    Second commit

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
-public class helloworld {
-    public static void main(String[] {
-        System.out.println("Hello, World");
-      }
-} 
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
